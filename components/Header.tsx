2f9a6980830377f0f70e79441c329fc55021d1f0
leurjim <shaqleao@icloud.com>
2024-11-30 04:16:49 -0500

    Mejorando el Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,15 @@
 import Link from "next/link";
 import ThemeSwitch from "./ThemeSwitch";
-
-const headerNavLinks = [
-    { href: '/', title: 'Home' },
-    { href: '/projects', title: 'Projects' },
-    { href: '/about', title: 'About' },
-  ]
+import headerNavLinks from "data/headerNavLinks"
+import MobileNav from "./MobileNav";
 
 export default function Header() {
     return (
         <header className="flex items-center w-full justify-between py-10">
             <Link href="/">
                 <div className="flex items-center justify-between">
-                    <div className="hidden h-6 text-2xl font-semibold sm:block">
-                        <svg className="relative flex h-10 w-52 ">
+                    <div className="hidden h-10 text-2xl font-semibold sm:block">
+                        <svg className="relative flex h-10 w-52">
                             <text className="fill-black dark:fill-white text-3xl animate-jump-in animate-normal" x="5" y="30">Leao Urbina</text>
                         </svg>
                     </div>
@@ -27,14 +23,15 @@ export default function Header() {
                         <Link
                             key={link.title}
                             href={link.href}
-                            className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                            className="block font-medium text-black hover:text-gray-500 dark:text-white dark:hover:text-gray-500"
                         >
                             {link.title}
                         </Link>
                     ))}
                 </div>
                 <ThemeSwitch />
-            </div>
+                <MobileNav />
+            </div>  
         </header>
     )
 }
--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,35 +1,54 @@
 'use client'
 
-import { FiSun, FiMoon } from "react-icons/fi"
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
-import Image from "next/image"
+
+const Sun = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    className="group:hover:text-gray-100 h-6 w-6"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+const Moon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    className="group:hover:text-gray-100 h-6 w-6"
+  >
+    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+  </svg>
+)
+const Blank = () => <svg className="h-6 w-6" />
 
 export default function ThemeSwitch() {
   const [mounted, setMounted] = useState(false)
-  const { setTheme, resolvedTheme } = useTheme()
+  const { theme, setTheme, resolvedTheme } = useTheme()
 
   useEffect(() =>  setMounted(true), [])
 
-  if (!mounted) return (
-    <Image
-      src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-      width={36}
-      height={36}
-      sizes="36x36"
-      alt="Loading Light/Dark Toggle"
-      priority={false}
-      title="Loading Light/Dark Toggle"
-      className="fill-white dark:fill-black"
-    />
+  return (
+    <div className="mr-5 flex items-center">
+      <div className="relative inline-block text-left">
+        <div className="flex items-center justify-center hover:text-primary-500 dark:hover:text-primary-400">
+          <button aria-label="Theme switcher"
+            className="px-5 py-2 hover:text-gray-500 text-black dark:text-white dark:hover:text-gray-500 font-semibold rounded-md"
+            onClick={() => {
+              setTheme(theme === 'light' ? 'dark' : 'light')
+            }}
+          >
+            {mounted ? resolvedTheme === 'dark' ? <Moon /> : <Sun /> : <Blank />}
+          </button>
+        </div>
+      </div>
+    </div>
   )
-
-  if (resolvedTheme === 'dark') {
-    return <FiSun onClick={() => setTheme('light')} />
-  }
-
-  if (resolvedTheme === 'light') {
-    return <FiMoon onClick={() => setTheme('dark')} />
-  }
-
 }
